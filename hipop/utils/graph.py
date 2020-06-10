c5b88fa4934fc84588edfb00cdca551850d6712d
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2020-06-10 10:29:44 +0200

    Fix subtypes when no types defined
    must contain at least 'object'

--- hipop/utils/graph.py
+++ hipop/utils/graph.py
@@ -5,6 +5,7 @@ import pddl
 
 def subtypes_closure(types: List[pddl.Type]) -> Dict[str, Set[str]]:
     graph = networkx.DiGraph()
+    graph.add_node('object')
     for typ in types:
         graph.add_edge(typ.type, typ.name)
         graph.add_edge('object', typ.type)
