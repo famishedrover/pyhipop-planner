dbc5fb149207e442be7ae48a28f33fda661597a5
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2020-06-11 21:52:42 +0200

    Test memory tracing

--- hipop/__main__.py
+++ hipop/__main__.py
@@ -1,12 +1,40 @@
 import sys
+import os
 import argparse
 import logging
 import time
+import linecache
+import tracemalloc
 
 import pddl
 from .problem import Problem
 
+
 LOGGER = logging.getLogger(__name__)
+
+
+def display_top(snapshot, key_type='lineno', limit=10):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type, cumulative=True)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, frame.filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
 
 
 def main():
@@ -26,6 +54,7 @@ def main():
                         level=args.loglevel,
                         format=logformat)
 
+
     tic = time.process_time()
     LOGGER.info("Parsing PDDL domain %s", args.domain)
     pddl_domain = pddl.parse_domain(args.domain, file_stream=True)
@@ -34,13 +63,19 @@ def main():
     toc = time.process_time()
     LOGGER.warning("parsing duration: %.3f", (toc - tic))
 
+    tracemalloc.start()
+
     tic = time.process_time()
     LOGGER.info("Building HiPOP problem")
     problem = Problem(pddl_problem, pddl_domain)
     toc = time.process_time()
     LOGGER.warning("building problem duration: %.3f", (toc - tic))
     LOGGER.info("nb actions: %d", len(problem.actions))
+    LOGGER.info("nb tasks: %d", len(problem.tasks))
+    LOGGER.info("init state size: %d", len(problem.init))
 
+    snapshot = tracemalloc.take_snapshot()
+    display_top(snapshot, limit=10)
 
 if __name__ == '__main__':
     main()
