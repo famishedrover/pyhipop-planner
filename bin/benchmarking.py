be7a561b14cc9075848432f782cb775335363590
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2020-07-02 23:29:33 +0200

    Multi-thread benchmarking.
    SHOP algo (and variants) ok on totally ordered problems. Fix #3
    Output format ok. Fix #5

--- bin/benchmarking.py
+++ bin/benchmarking.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import argparse
 from collections import defaultdict
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 import pddl
 from hipop.problem.problem import Problem
@@ -35,100 +36,120 @@ def setup():
 
 class SolveThread(threading.Thread):
 
-    def __init__(self, domain, problem, alg, results):
+    def __init__(self, problem, alg, stats):
         threading.Thread.__init__(self)
-        self.domain = domain
         self.problem = problem
         self.alg = alg
-        self.results = results
+        self.stats = stats
+        self.stats.alg = alg
 
     def terminate(self):
-        self.shop.stop()
-        self.join()
+        try:
+            self.shop.stop()
+            self.join()
+        except AttributeError:
+            pass
 
     def run(self):
-        tic = time.process_time()
-        LOGGER.info("Parsing PDDL domain %s", self.domain)
-        pddl_domain = pddl.parse_domain(self.domain, file_stream=True)
-        LOGGER.info("Parsing PDDL problem %s", self.problem)
-        pddl_problem = pddl.parse_problem(self.problem, file_stream=True)
-        toc = time.process_time()
-        LOGGER.info("parsing duration: %.3f", (toc - tic))
-
-        stats = Statistics(pddl_domain.name, pddl_problem.name, self.alg)
-        stats.parsing_time = (toc - tic)
-
-        tic = time.process_time()
-        LOGGER.info("Building HiPOP problem")
-        shop_problem = Problem(pddl_problem, pddl_domain,
-                               filter_static=True, tdg_filter_useless=True,
-                               htn_problem=True)
-        toc = time.process_time()
-        stats.problem_time = (toc - tic)
-        LOGGER.info("building problem duration: %.3f", (toc - tic))
-
         LOGGER.info("Solving problem with SHOP")
         tic = time.process_time()
         if self.alg.lower() == 'shop':
-            self.shop = SHOP(shop_problem, no_duplicate_search=True, hierarchical_plan=False)
+            self.shop = SHOP(self.problem, no_duplicate_search=True, hierarchical_plan=False)
             output = output_ipc2020_flat
         elif self.alg.lower() == 'hshop':
-            self.shop = SHOP(shop_problem, no_duplicate_search=True,
+            self.shop = SHOP(self.problem, no_duplicate_search=True,
                         hierarchical_plan=True, poset_inc_impl=False)
             output = output_ipc2020_hierarchical
-        elif self.alg.lower() == 'hshop-inc':
-            self.shop = SHOP(shop_problem, no_duplicate_search=True,
+        elif self.alg.lower() == 'hshopi':
+            self.shop = SHOP(self.problem, no_duplicate_search=True,
                         hierarchical_plan=True, poset_inc_impl=True)
             output = output_ipc2020_hierarchical
-        plan = self.shop.find_plan(shop_problem.init, shop_problem.goal_task)
+        plan = self.shop.find_plan(self.problem.init, self.problem.goal_task)
         toc = time.process_time()
-        stats.solving_time = (toc - tic)
+        self.stats.solving_time = (toc - tic)
         LOGGER.info("SHOP solving duration: %.3f", (toc - tic))
 
         if plan is None:
             LOGGER.error("No plan found!")
             return
         #else:
-        out_plan = open("plan.plan", "w", encoding="utf-8")
+        out_plan = open(f"plan-{self.alg.lower()}.plan", "w", encoding="utf-8")
         output(plan, out_plan)
         out_plan.close()
 
-        verificator = subprocess.Popen(["../pandaPIparser/pandaPIparser",
-                                        "-verify",
-                                        self.domain,
-                                        self.problem,
-                                        "plan.plan"], stdout=subprocess.PIPE)
-        verification = verificator.stdout.read().decode(encoding='utf-8')
-        stats.verif = verification.count("true")
-        self.results.append(stats)
+def build_problem(domain, problem):
+    tic = time.process_time()
+    LOGGER.info("Parsing PDDL domain %s", domain)
+    pddl_domain = pddl.parse_domain(domain, file_stream=True)
+    LOGGER.info("Parsing PDDL problem %s", problem)
+    pddl_problem = pddl.parse_problem(problem, file_stream=True)
+    toc = time.process_time()
+    LOGGER.info("parsing duration: %.3f", (toc - tic))
+    stats = Statistics(pddl_domain.name, pddl_problem.name, '')
+    stats.parsing_time = (toc - tic)
+    tic = time.process_time()
+    LOGGER.info("Building HiPOP problem")
+    shop_problem = Problem(pddl_problem, pddl_domain,
+                           filter_static=True, tdg_filter_useless=True,
+                           htn_problem=True)
+    toc = time.process_time()
+    stats.problem_time = (toc - tic)
+    LOGGER.info("building problem duration: %.3f", (toc - tic))
+    return shop_problem, stats
 
-def process_alg(domain, problem, algorithm, data, timeout):
-    alg_thread = SolveThread(domain, problem, algorithm, data)
-    alg_thread.start()
-    alg_thread.join(timeout=timeout)
-    if alg_thread.is_alive():
-        LOGGER.error("Alg %s timed-out on problem %s", algorithm, problem)
-        alg_thread.terminate()
-        data.append(Statistics(domain.name, problem.name, algorithm))
-    print(data[-1])
+def verify(domain, problem, plan):
+    verificator = subprocess.Popen(["../pandaPIparser/pandaPIparser",
+                                    "-verify",
+                                    domain,
+                                    problem,
+                                    plan], stdout=subprocess.PIPE)
+    verification = verificator.stdout.read().decode(encoding='utf-8')
+    return verification.count("true")
+
+
+def process_problem(pddl_domain, pddl_problem, problem, timeout, stats):
+    results = [deepcopy(stats), deepcopy(stats), deepcopy(stats)]
+    shop_thread = SolveThread(problem, 'shop', results[0])
+    hshop_thread = SolveThread(problem, 'hshop', results[1])
+    hshopi_thread = SolveThread(problem, 'hshopi', results[2])
+    shop_thread.start()
+    hshop_thread.start()
+    hshopi_thread.start()
+    hshop_thread.join(timeout=timeout)
+    shop_thread.join()
+    hshopi_thread.join()
+    if shop_thread.is_alive():
+        LOGGER.error("SHOP timed-out on problem %s", problem.name)
+        shop_thread.terminate()
+    else:
+        results[0].verif = verify(pddl_domain, pddl_problem, 'plan-shop.plan')
+    if hshop_thread.is_alive():
+        LOGGER.error("HSHOP timed-out on problem %s", problem.name)
+        hshop_thread.terminate()
+    else:
+        results[1].verif = verify(pddl_domain, pddl_problem, 'plan-hshop.plan')
+    if hshopi_thread.is_alive():
+        LOGGER.error("HSHOP-INC timed-out on problem %s", problem.name)
+        hshopi_thread.terminate()
+    else:
+        results[2].verif = verify(pddl_domain, pddl_problem, 'plan-hshopi.plan')
+    for r in results: print(r)
+    return results
 
 def process_domain(benchmark, bench_root, max_bench, timeout):
     root = os.path.join(bench_root, benchmark)
     domain = next(Path(os.path.join(root, 'domains')).rglob('*.?ddl'))
     bench = 1
-    datas = defaultdict(list)
-    datas['shop'] = []
-    datas['hshop'] = []
-    datas['hshop-inc'] = []
+    results = []
     problems = []
     for problem in sorted(Path(os.path.join(root, 'problems')).rglob('*.?ddl')):
         problems.append(problem)
-        for alg, data in datas.items():
-            process_alg(domain, problem, alg, data, timeout)
+        pb, stats = build_problem(domain, problem)
+        results.append(process_problem(domain, problem, pb, timeout, stats))
         bench += 1
         if bench > max_bench:
             break
-    return problems, datas
+    return problems, results
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="SHOP planner")
@@ -149,9 +170,9 @@ if __name__ == '__main__':
         bench_root = os.path.join('..', 'benchmarks', 'ipc2020-hierarchical', 'HDDL-total')
     problems, results = process_domain(args.benchmark, bench_root, args.nb_problems, args.timeout)
     if args.plot:
-        plt.plot(range(len(problems)), [x.solving_time for x in results['shop']], 'r-x', label="SHOP")
-        plt.plot(range(len(problems)), [x.solving_time for x in results['hshop']], 'b-o', label="H-SHOP")
-        plt.plot(range(len(problems)), [x.solving_time for x in results['hshop-inc']], 'g-s', label="H-SHOP-INC")
+        plt.plot(range(len(problems)), [(x[0].solving_time if x[0].verif else None) for x in results], 'r-x', label="SHOP")
+        plt.plot(range(len(problems)), [(x[1].solving_time if x[1].verif else None) for x in results], 'b-o', label="H-SHOP")
+        plt.plot(range(len(problems)), [(x[2].solving_time if x[2].verif else None) for x in results], 'g-s', label="H-SHOP-INC")
         plt.xticks([x for x in range(len(problems))], [f"{x+1}" for x in range(len(problems))])
         plt.xlabel("problem")
         plt.ylabel("solving time (s)")
